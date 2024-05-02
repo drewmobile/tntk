3c54a9e1a601c0e8ea534a6193a6520d82c382ec
drewmobile <drew@blockvest.net>
2024-05-02 18:30:26 -0500

    init

--- vpc/bash/vpc.sh
+++ vpc/bash/vpc.sh
@@ -27,15 +27,15 @@
 #   MODIFY THE SETTINGS BELOW
 #==============================================================================
 #
-AWS_REGION="us-west-1"
+AWS_REGION="us-east-1"
 VPC_NAME="My VPC"
 VPC_CIDR="10.0.0.0/16"
 SUBNET_PUBLIC_CIDR="10.0.1.0/24"
-SUBNET_PUBLIC_AZ="us-west-1a"
-SUBNET_PUBLIC_NAME="10.0.1.0 - us-west-1a"
+SUBNET_PUBLIC_AZ="us-east-1a"
+SUBNET_PUBLIC_NAME="10.0.1.0 - us-east-1a"
 SUBNET_PRIVATE_CIDR="10.0.2.0/24"
-SUBNET_PRIVATE_AZ="us-west-1c"
-SUBNET_PRIVATE_NAME="10.0.2.0 - us-west-1b"
+SUBNET_PRIVATE_AZ="us-east-1c"
+SUBNET_PRIVATE_NAME="10.0.2.0 - us-east-1b"
 CHECK_FREQUENCY=5
 #
 #==============================================================================
